8deeeb4ffb41a659a1d58b7b9fb69e557c54e17b
ana301 <31302345+ana301@users.noreply.github.com>
2020-09-10 21:56:16 +0530

    Sender Test removed as it was not named correctly

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,31 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sender
 {
-
-    interface IDataReader
-    {
-        void ReadData();
-    }
-
-    class DataReader : IDataReader
-    {
-       
-        public void ReadData()
-        {
-            Console.WriteLine("heyy");
-        }
-    }
-
     class Program
-    {   
-
-
+    {
         static void Main(string[] args)
         {
         }
