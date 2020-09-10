ed34b3a4b9bc88d120f436a8ae1d857a6162c1e1
ana301 <31302345+ana301@users.noreply.github.com>
2020-09-10 20:36:39 +0530

    Sender Test Project added

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sender
 {
-    class Program
+
+    interface IDataReader
     {
+        void ReadData();
+    }
+
+    class DataReader : IDataReader
+    {
+       
+        public void ReadData()
+        {
+            Console.WriteLine("heyy");
+        }
+    }
+
+    class Program
+    {   
+
+
         static void Main(string[] args)
         {
         }
