35e79527875410ed2ab02e3c19d40c568c5997f1
atuljha2524 <atuljha2524@gmail.com>
2020-09-11 13:38:26 +0530

    data record added

---
+++ Receiver/DataRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class DataRecord
+    {
+        private DateTime T;
+        private int temperatue;
+        private int humidity;
+        
+    }
+}
