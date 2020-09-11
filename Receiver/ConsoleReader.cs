c5337515ac0d4c2cfe025ee795630e9b2db21185
atuljha2524 <atuljha2524@gmail.com>
2020-09-11 17:21:35 +0530

    classes added

---
+++ Receiver/ConsoleReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class ConsoleReader
+    {
+        public DataRecord data;
+        public DataRecord ReadConsole()
+        {
+            string val;
+            val = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Receiver/DataRecord.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    public class DataRecord
+    {
+        private DateTime time;
+        private int temperature;
+        private int humidity;
+
+        public DateTime Time
+        {
+            get {
+                return time;
+            } set {
+                time = value;
+            }
+        }
+
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                temperature = value;
+            }
+        }
+
+        public int Humidity
+        {
+            get
+            {
+                return humidity;
+            }
+            set
+            {
+                humidity = value;
+            }
+        }
+
+
+    }
+}
