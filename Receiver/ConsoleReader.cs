7ccd117d408d0cc7612d3125af1fd38e4a6d5ab7
atuljha2524 <atuljha2524@gmail.com>
2020-09-12 16:26:04 +0530

    Receiver Completed

--- Receiver/ConsoleReader.cs
+++ Receiver/ConsoleReader.cs
@@ -8,16 +8,18 @@ namespace Receiver
 {
     class ConsoleReader
     {
-        public DataRecord data;
+        public DataRecord data = new DataRecord();
+
+
         public DataRecord ReadConsole()
         {
-            string val;
-            val = Console.ReadLine();
-            string[] lines = val.Split(new[] { Environment.NewLine },StringSplitOptions.None);
-            data.Time = Convert.ToDateTime(lines[0]);
-            data.Temperature = Convert.ToInt32(lines[1]);
-            data.Humidity = Convert.ToInt32(lines[2]);
-            return data;
+            string line;
+            line = Console.ReadLine();
+            var lines = line.Split(',').ToList();
+            this.data.Time = Convert.ToDateTime(lines[0]);
+            this.data.Temperature = Convert.ToInt32(lines[1]);
+            this.data.Humidity = Convert.ToInt32(lines[2]);
+            return this.data;
         }
     }
 }
--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Receiver
 {
     class Program
     {
+        private static Timer aTimer;
         static void Main(string[] args)
         {
-            Console.WriteLine("atul\n");
-            string x = Console.ReadLine();
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = 2000;
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            aTimer.AutoReset = true;
+
+            // Start the timer
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            ConsoleReader consoleReader = new ConsoleReader();
+            DataRecord dataRecord = consoleReader.ReadConsole();
+            IAlerter alerter = new EmailAlerter();
+            Checker checker = new Checker(alerter);
+            checker.Check(dataRecord);
         }
     }
 }
